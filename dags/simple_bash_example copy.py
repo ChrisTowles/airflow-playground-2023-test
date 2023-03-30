13d6f3178b530630c8eccb51e805c02b1268730f
Chris Towles <Chris.Towles@gmail.com>
2023-03-29 22:32:39 -0400

    chore: setup postgres connection in airflow

---
+++ dags/simple_bash_example copy.py
@@ -0,0 +1,34 @@
+
+
+from __future__ import annotations
+
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="simple_bash_example",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["simple"],
+    params={},
+) as dag:
+
+    
+    run_this: BashOperator = BashOperator(
+        task_id="run_this_1",
+        bash_command="echo 1",
+    )
+    
+
+    run_this
+
+
+if __name__ == "__main__":
+    dag.test()
